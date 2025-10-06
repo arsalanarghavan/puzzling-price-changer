7d80388a9544d96bd6086d4abe232c42f82fcd9b
Arsalan Arghavan <arsalanarghavan2014@gmail.com>
2025-10-06 14:55:52 +0330

    .

--- assets/js/price-manager.js
+++ assets/js/price-manager.js
@@ -8,62 +8,90 @@ jQuery(document).ready(function($) {
         return str.toString().replace(/[^0-9]/g, '');
     }
 
-    $('.variation-price-input').on('input', function() {
-        var cursorPosition = this.selectionStart;
-        var originalLength = this.value.length;
-        
-        var value = $(this).val();
-        var unformattedValue = unformatNumber(value);
-        
-        if (unformattedValue.length > 0) {
-            var formattedValue = formatNumber(unformattedValue);
-            $(this).val(formattedValue);
+    // This function re-attaches the event listeners to the new price inputs
+    function rebindPriceInputEvents() {
+        $('.variation-price-input').off('input change'); // Unbind previous events to avoid duplicates
+
+        $('.variation-price-input').on('input', function() {
+            var cursorPosition = this.selectionStart;
+            var originalLength = this.value.length;
             
-            var newLength = this.value.length;
-            cursorPosition = cursorPosition + (newLength - originalLength);
-            this.setSelectionRange(cursorPosition, cursorPosition);
-        } else {
-            $(this).val('');
-        }
-    });
+            var value = $(this).val();
+            var unformattedValue = unformatNumber(value);
+            
+            if (unformattedValue.length > 0) {
+                var formattedValue = formatNumber(unformattedValue);
+                $(this).val(formattedValue);
+                
+                var newLength = this.value.length;
+                cursorPosition = cursorPosition + (newLength - originalLength);
+                this.setSelectionRange(cursorPosition, cursorPosition);
+            } else {
+                $(this).val('');
+            }
+        });
 
-    $('.variation-price-input').on('change', function() {
-        
-        var inputField = $(this);
-        var variationId = inputField.data('variation-id');
-        var formattedPrice = inputField.val();
-        var newPrice = unformatNumber(formattedPrice);
-        
-        var saveStatus = inputField.closest('.price-wrapper').find('.save-status');
+        $('.variation-price-input').on('change', function() {
+            var inputField = $(this);
+            var variationId = inputField.data('variation-id');
+            var formattedPrice = inputField.val();
+            var newPrice = unformatNumber(formattedPrice);
+            
+            var saveStatus = inputField.closest('.price-wrapper').find('.save-status');
+            saveStatus.removeClass('success error').addClass('saving');
 
-        saveStatus.removeClass('success error').addClass('saving');
+            var data = {
+                action: 'update_variation_price',
+                nonce: psp_ajax_object.update_nonce,
+                variation_id: variationId,
+                new_price: newPrice 
+            };
+
+            $.post(psp_ajax_object.ajax_url, data, function(response) {
+                saveStatus.removeClass('saving');
+                if (response.success) {
+                    saveStatus.addClass('success');
+                } else {
+                    saveStatus.addClass('error');
+                    alert('خطا: ' + response.data.message);
+                }
+                
+                setTimeout(function() {
+                    saveStatus.removeClass('success error');
+                }, 2000);
+            }).fail(function() {
+                saveStatus.removeClass('saving').addClass('error');
+                alert('خطای ارتباط با سرور. لطفا اتصال اینترنت خود را بررسی کنید.');
+                setTimeout(function() {
+                    saveStatus.removeClass('error');
+                }, 2000);
+            });
+        });
+    }
+
+    // Filter functionality
+    function applyFilters() {
+        var category = $('#psp_category_filter').val();
+        var brand = $('#psp_brand_filter').val();
+        var tableBody = $('#psp-price-list-body');
+
+        tableBody.html('<tr><td colspan="3"><span class="spinner is-active" style="float:right; margin: 10px;"></span></td></tr>');
 
         var data = {
-            action: 'update_variation_price',
-            nonce: psp_ajax_object.nonce,
-            variation_id: variationId,
-            new_price: newPrice 
+            action: 'psp_get_filtered_products',
+            nonce: psp_ajax_object.filter_nonce,
+            category: category,
+            brand: brand
         };
 
         $.post(psp_ajax_object.ajax_url, data, function(response) {
-            saveStatus.removeClass('saving');
-            if (response.success) {
-                saveStatus.addClass('success');
-            } else {
-                saveStatus.addClass('error');
-                alert('خطا: ' + response.data.message);
-            }
-            
-            setTimeout(function() {
-                saveStatus.removeClass('success error');
-            }, 2000);
-
-        }).fail(function() {
-            saveStatus.removeClass('saving').addClass('error');
-            alert('خطای ارتباط با سرور. لطفا اتصال اینترنت خود را بررسی کنید.');
-            setTimeout(function() {
-                saveStatus.removeClass('error');
-            }, 2000);
+            tableBody.html(response);
+            rebindPriceInputEvents(); // Re-attach events to the new inputs
         });
-    });
+    }
+
+    $('#psp_category_filter, #psp_brand_filter').on('change', applyFilters);
+
+    // Initial bind
+    rebindPriceInputEvents();
 });
--- my-price-manager.php
+++ my-price-manager.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name:       Puzzling Steel Price List
+ * Plugin Name:       تغییر قیمت‌ها
  * Description:       A page to manage all WooCommerce variable product prices using AJAX.
- * Version:           0.0.4
+ * Version:           0.1.0
  * Author:            Arsalan Arghavan
  */
 
@@ -12,13 +12,22 @@ if (!defined('ABSPATH')) {
 
 function psp_add_admin_menu() {
     add_menu_page(
-        'لیست قیمت',
-        'لیست قیمت',
+        'پازلینگ',
+        'پازلینگ',
+        'manage_woocommerce',
+        'puzzling_main_menu',
+        null,
+        'dashicons-admin-generic',
+        55
+    );
+
+    add_submenu_page(
+        'puzzling_main_menu',
+        'تغییر قیمت‌ها',
+        'تغییر قیمت‌ها',
         'manage_woocommerce',
         'steel_price_manager',
-        'psp_render_admin_page',
-        'dashicons-editor-table',
-        56
+        'psp_render_admin_page'
     );
 }
 add_action('admin_menu', 'psp_add_admin_menu');
@@ -29,6 +38,32 @@ function psp_render_admin_page() {
         <h1><span class="dashicons-before dashicons-editor-table"></span>لیست قیمت</h1>
         <p>در این صفحه می‌توانید قیمت تمام متغیرهای محصولات را به سرعت تغییر دهید. پس از تغییر قیمت، به صورت خودکار ذخیره می‌شود.</p>
         
+        <div class="psp-filters">
+            <?php
+            // Category Filter
+            wp_dropdown_categories(array(
+                'show_option_all' => 'همه دسته‌بندی‌ها',
+                'taxonomy'        => 'product_cat',
+                'name'            => 'psp_category_filter',
+                'id'              => 'psp_category_filter',
+                'class'           => 'psp-filter-select',
+                'hierarchical'    => true,
+                'value_field'     => 'slug',
+            ));
+
+            // Brand Filter (Perfect Brands for WooCommerce)
+            $brands = get_terms(array('taxonomy' => 'pwb-brand', 'hide_empty' => false));
+            if (!is_wp_error($brands) && !empty($brands)) {
+                echo '<select id="psp_brand_filter" name="psp_brand_filter" class="psp-filter-select">';
+                echo '<option value="">همه برندها</option>';
+                foreach ($brands as $brand) {
+                    echo '<option value="' . esc_attr($brand->slug) . '">' . esc_html($brand->name) . '</option>';
+                }
+                echo '</select>';
+            }
+            ?>
+        </div>
+
         <table class="wp-list-table widefat fixed striped psp-table">
             <thead>
                 <tr>
@@ -37,70 +72,108 @@ function psp_render_admin_page() {
                     <th class="price-col">قیمت (تومان)</th>
                 </tr>
             </thead>
-            <tbody>
-                <?php
-                $args = array('type' => 'variable', 'limit' => -1, 'return' => 'ids');
-                $variable_products_ids = wc_get_products($args);
-
-                if (empty($variable_products_ids)) {
-                    echo '<tr><td colspan="3">هیچ محصول متغیری یافت نشد.</td></tr>';
-                } else {
-                    foreach ($variable_products_ids as $product_id) {
-                        $product = wc_get_product($product_id);
-                        $variations = $product->get_available_variations();
-
-                        foreach ($variations as $variation) {
-                            $variation_obj = wc_get_product($variation['variation_id']);
-                            $price = $variation_obj->get_price();
-                            ?>
-                            <tr data-variation-id="<?php echo esc_attr($variation['variation_id']); ?>">
-                                <td><strong><?php echo esc_html($product->get_name()); ?></strong></td>
-                                <td class="attributes-cell">
-                                    <?php
-                                    foreach ($variation['attributes'] as $attribute_slug => $term_slug) {
-                                        if (empty($term_slug)) continue;
-                                        $decoded_term_slug = urldecode($term_slug);
-                                        $taxonomy = str_replace('attribute_', '', urldecode($attribute_slug));
-                                        $attribute_label = wc_attribute_label($taxonomy);
-                                        $term = get_term_by('slug', $decoded_term_slug, $taxonomy);
-                                        $term_name = $term ? $term->name : $decoded_term_slug;
-
-                                        echo '<span class="attribute-tag">' . esc_html($attribute_label) . ': <strong>' . esc_html($term_name) . '</strong></span>';
-                                    }
-                                    ?>
-                                </td>
-                                <td>
-                                    <div class="price-wrapper">
-                                        <input type="text" 
-                                               class="variation-price-input" 
-                                               value="<?php echo esc_attr(number_format($price)); ?>" 
-                                               data-variation-id="<?php echo esc_attr($variation['variation_id']); ?>"
-                                               placeholder="قیمت جدید...">
-                                        <span class="save-status">
-                                            <span class="spinner"></span>
-                                            <span class="status-icon"></span>
-                                        </span>
-                                    </div>
-                                </td>
-                            </tr>
-                            <?php
-                        }
-                    }
-                }
-                ?>
+            <tbody id="psp-price-list-body">
+                <?php psp_get_filtered_products_callback(false); // Initial load ?>
             </tbody>
         </table>
     </div>
     <?php
 }
 
+function psp_get_filtered_products_callback($ajax = true) {
+    if ($ajax) {
+        check_ajax_referer('psp_filter_nonce', 'nonce');
+    }
+
+    $category_slug = isset($_POST['category']) ? sanitize_text_field($_POST['category']) : '';
+    $brand_slug = isset($_POST['brand']) ? sanitize_text_field($_POST['brand']) : '';
+
+    $tax_query = array('relation' => 'AND');
+
+    if (!empty($category_slug)) {
+        $tax_query[] = array(
+            'taxonomy' => 'product_cat',
+            'field'    => 'slug',
+            'terms'    => $category_slug,
+        );
+    }
+    if (!empty($brand_slug)) {
+        $tax_query[] = array(
+            'taxonomy' => 'pwb-brand',
+            'field'    => 'slug',
+            'terms'    => $brand_slug,
+        );
+    }
+
+    $args = array(
+        'type' => 'variable',
+        'limit' => -1,
+        'return' => 'ids',
+        'tax_query' => $tax_query,
+    );
+    $variable_products_ids = wc_get_products($args);
+
+    if (empty($variable_products_ids)) {
+        echo '<tr><td colspan="3">هیچ محصول متغیری یافت نشد.</td></tr>';
+    } else {
+        foreach ($variable_products_ids as $product_id) {
+            $product = wc_get_product($product_id);
+            $variations = $product->get_available_variations();
+
+            foreach ($variations as $variation) {
+                $variation_obj = wc_get_product($variation['variation_id']);
+                $price = $variation_obj->get_price();
+                ?>
+                <tr data-variation-id="<?php echo esc_attr($variation['variation_id']); ?>">
+                    <td><strong><?php echo esc_html($product->get_name()); ?></strong></td>
+                    <td class="attributes-cell">
+                        <?php
+                        foreach ($variation['attributes'] as $attribute_slug => $term_slug) {
+                            if (empty($term_slug)) continue;
+                            $decoded_term_slug = urldecode($term_slug);
+                            $taxonomy = str_replace('attribute_', '', urldecode($attribute_slug));
+                            $attribute_label = wc_attribute_label($taxonomy);
+                            $term = get_term_by('slug', $decoded_term_slug, $taxonomy);
+                            $term_name = $term ? $term->name : $decoded_term_slug;
+
+                            echo '<span class="attribute-tag">' . esc_html($attribute_label) . ': <strong>' . esc_html($term_name) . '</strong></span>';
+                        }
+                        ?>
+                    </td>
+                    <td>
+                        <div class="price-wrapper">
+                            <input type="text" 
+                                   class="variation-price-input" 
+                                   value="<?php echo esc_attr(number_format($price)); ?>" 
+                                   data-variation-id="<?php echo esc_attr($variation['variation_id']); ?>"
+                                   placeholder="قیمت جدید...">
+                            <span class="save-status">
+                                <span class="spinner"></span>
+                                <span class="status-icon"></span>
+                            </span>
+                        </div>
+                    </td>
+                </tr>
+                <?php
+            }
+        }
+    }
+
+    if ($ajax) {
+        wp_die();
+    }
+}
+add_action('wp_ajax_psp_get_filtered_products', 'psp_get_filtered_products_callback');
+
+
 function psp_enqueue_admin_scripts($hook) {
-    if ('toplevel_page_steel_price_manager' != $hook) return;
-    wp_enqueue_style('psp-admin-styles', plugin_dir_url(__FILE__) . 'assets/css/price-manager.css', [], '1.2');
-    wp_enqueue_script('psp-admin-script', plugin_dir_url(__FILE__) . 'assets/js/price-manager.js', array('jquery'), '1.2', true);
+    if ('puzzling_page_steel_price_manager' != $hook) return;
+    wp_enqueue_style('psp-admin-styles', plugin_dir_url(__FILE__) . 'assets/css/price-manager.css', [], '1.3');
+    wp_enqueue_script('psp-admin-script', plugin_dir_url(__FILE__) . 'assets/js/price-manager.js', array('jquery'), '1.3', true);
     wp_localize_script('psp-admin-script', 'psp_ajax_object', array(
         'ajax_url' => admin_url('admin-ajax.php'),
-        'nonce'    => wp_create_nonce('psp_update_price_nonce')
+        'update_nonce' => wp_create_nonce('psp_update_price_nonce'),
+        'filter_nonce' => wp_create_nonce('psp_filter_nonce'),
     ));
 }
 add_action('admin_enqueue_scripts', 'psp_enqueue_admin_scripts');
